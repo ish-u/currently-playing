22c4eaccff7b8c1c81300176801c8315e0ddef8a
Anmol Gupta <anmolgupta520@gmail.com>
2024-11-08 22:41:05 +0530

    feat: add favicon

--- main.go
+++ main.go
@@ -219,6 +219,7 @@ func currently_playing_page(w http.ResponseWriter, req *http.Request) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{.Name}}</title>
+		<link rel="icon" type="image/x-icon" href="{{.Image}}">
     </head>
 	<style>
         body {
